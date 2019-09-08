853166e472e196b955a97c931fd990c7ca71679b
fitzix <caojunkaiv@gmail.com>
2019-09-08 13:21:39 +0800

    feat:

--- src/components/AppList.vue
+++ src/components/AppList.vue
@@ -2,7 +2,7 @@
   <el-row :gutter="5">
     <el-col :xs="8" :sm="6" :md="6" :lg="4" :xl="4" v-for="o in 10" :key="o" class="asn-app-list">
       <el-card :body-style="{ padding: '10px' }">
-        <img src="https://img.xclient.info/attachment/2019/07/3b220c11-f6eb-3312-cd32-39e96d76a830.png" class="asn-app-list_icon" alt="" />
+        <img src="https://img.xclient.info/attachment/2019/07/3b220c11-f6eb-3312-cd32-39e96d76a830.png" class="asn-app-list_icon" alt />
         <div class="asn-app-list_status">
           <i class="el-icon-s-data">2323</i>
           <i class="el-icon-time">2019-08-08</i>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,18 +1,20 @@
 <template>
   <el-container>
-    <el-header>
+    <el-header height="61px">
       <div class="asn-nav">
         <a href="" class="asn-nav-logo">
           <img src="https://cdn-ossd.zipjpg.com/reg/bd6bde94290bc6fc0a5687afa982b543_2_2_art.png" alt="" />
         </a>
-        <div class="asn-nav-bar">
-          <a href="" class="asn-nav-item">首页</a>
-          <a href="" class="asn-nav-item">应用</a>
-          <a href="" class="asn-nav-item">书籍</a>
-          <el-input v-model="searchKey" placeholder="请输入内容" size="medium" class="asn-nav-search">
-            <i class="el-icon-search el-input__icon" slot="suffix" @click="x"></i>
-          </el-input>
-        </div>
+        <el-menu :default-active="activeIndex" mode="horizontal">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">应用</el-menu-item>
+          <el-menu-item index="3">书籍</el-menu-item>
+          <el-menu-item>
+            <el-input v-model="searchKey" placeholder="请输入内容" size="medium" class="asn-nav-search">
+              <i class="el-icon-search el-input__icon" slot="suffix" @click="x"></i>
+            </el-input>
+          </el-menu-item>
+        </el-menu>
       </div>
     </el-header>
     <el-main>
@@ -29,7 +31,7 @@ export default {
   data() {
     return {
       searchKey: '',
-      activeIndex: 1,
+      activeIndex: '1',
     };
   },
   methods: {
