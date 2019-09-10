bcc066ad1ca6fdfa7691ad9863b57d9b42dbbccf
fitzix <caojunkaiv@gmail.com>
2019-09-10 21:59:03 +0800

    feat:

--- src/utils/common/markdown.js
+++ src/utils/common/markdown.js
@@ -4,5 +4,5 @@ import MarkdownIt from 'markdown-it';
 export function render(str) {
   // return marked(str);
   let md = new MarkdownIt();
-  md.render(str);
+  return md.render(str);
 }
--- src/views/app/index.vue
+++ src/views/app/index.vue
@@ -36,7 +36,7 @@
           <div class="status">
             <p class="hot-name">热度</p>
             <p class="hot-number">3243</p>
-            <a class="download" href="">下载</a>
+            <a class="download" href="#asn-history-version">下载</a>
             <a class="donate" href="">赞助</a>
             <p class="info-title">基本信息</p>
             <div class="info">
@@ -51,20 +51,84 @@
         <el-col :xs="24" :sm="16">
           <div v-html="render" class="markdown-section"></div>
         </el-col>
-        <el-col :xs="24" :sm="8"></el-col>
+        <el-col :xs="24" :sm="8">
+          <asn-hot-app :data="hotData" class="hot" />
+        </el-col>
       </el-row>
+      <div class="content-version" id="asn-history-version">
+        <p>历史版本</p>
+        <table>
+          <thead>
+            <tr>
+              <th>版本号</th>
+              <th>语言</th>
+              <th>更新时间</th>
+              <th>文件大小</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>3.3.4</td>
+              <td>英文</td>
+              <td>2019-09-10</td>
+              <td>37.8M</td>
+              <td class="down-cell">
+                <a class="download" href="">百度云</a>
+                <a class="download" href="">Google Drive</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import AsnHotApp from 'src/components/app/hot';
 import axios from 'src/utils/common/http';
 import { render } from 'src/utils/common/markdown';
+
 export default {
   name: 'AsnAppInfo',
   data() {
     return {
       mdContent: '',
+      hotData: [
+        {
+          id: 10,
+          name: 'Sketch 57.1 轻量易用的矢量设计工具',
+          hot: 1212121,
+          update_at: '2017-12-08T00:00:00.00Z',
+          description: 'Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求。Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求',
+        },
+        {
+          id: 11,
+          hot: 1212121,
+          description: 'Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求。Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求',
+        },
+        {
+          id: 12,
+          hot: 1212,
+          description: 'Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求。Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求',
+        },
+        {
+          id: 13,
+          hot: 12122,
+          description: 'Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求。Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求',
+        },
+        {
+          id: 14,
+          hot: 12,
+          description: 'Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求。Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求',
+        },
+        {
+          id: 15,
+          hot: 1212,
+          description: 'Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求。Lungo是一款防止Mac屏幕进入休眠状态的软件，可以满足我们让mac电脑不进入休眠状态的需求',
+        },
+      ],
     };
   },
   computed: {
@@ -72,8 +136,11 @@ export default {
       return render(this.mdContent);
     },
   },
+  components: {
+    AsnHotApp,
+  },
   created() {
-    axios.get('https://raw.githubusercontent.com/fitzix/fitzix.github.io/pages/source/_posts/2018-08-16-channel%E5%BA%95%E5%B1%82%E5%8E%9F%E7%90%86.md').then(resp => {
+    axios.get('https://raw.githubusercontent.com/segmentio/kafka-go/master/README.md').then(resp => {
       this.mdContent = resp;
     });
   },
